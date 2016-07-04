51f6d67047bf989f1937a2439de70961b2a74006
Klemen Istenic <klemen.istenic@gmail.com>
2016-07-04 15:21:12 +0200

    Computed uncertainty of the reconstruction - part2

---
+++ src/openMVG/sfm/sfm_uncertainty.hpp
@@ -0,0 +1,112 @@
+// Copyright (c) 2015 Pierre Moulon.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef OPENMVG_SFM_UNCERTAINTY_HPP
+#define OPENMVG_SFM_UNCERTAINTY_HPP
+
+#include "openMVG/types.hpp"
+#include "openMVG/numeric/numeric.h"
+
+#include <cereal/cereal.hpp> // Serialization
+
+namespace openMVG {
+namespace sfm {
+
+
+/// Define a landmark (a 3D point, with it's 2d observations)
+struct UncertaintyLandmark
+{
+  Eigen::Matrix3d covariance;
+
+  // Serialization
+  template <class Archive>
+  void save( Archive & ar) const
+  {
+    const std::vector<double> cov = { covariance(0,0), covariance(0,1), covariance(0,2), covariance(1,0), covariance(1,1), covariance(1,2), covariance(2,0), covariance(2,1), covariance(2,2) };
+    ar(cereal::make_nvp("covariance", cov ));
+  }
+
+  template <class Archive>
+  void load( Archive & ar)
+  {
+    std::vector<double> cov(9);
+    ar(cereal::make_nvp("covariance", cov ));
+    covariance = Eigen::Map<const Eigen::Matrix3d>(&cov[0]);
+  }
+};
+
+typedef Eigen::SparseMatrix<double, Eigen::RowMajor> EigenSparseMatrix;
+
+struct UncertaintyCams
+{
+  EigenSparseMatrix covariance;
+
+  // Serialization
+  template <class Archive>
+  void save( Archive & ar) const
+  {
+	const int n_rows = covariance.rows();
+	const int n_cols = covariance.cols();
+	ar(cereal::make_nvp("rows", n_rows ));
+	ar(cereal::make_nvp("cols", n_cols ));
+	
+	// CRS Matrix
+	std::vector<int> m_rows;
+	std::vector<int> m_cols;
+	std::vector<double>m_values;
+	
+	int n_element = 0;
+	for(int i=0; i < n_rows; ++i) {
+	  m_rows.push_back(n_element);
+	  for(typename EigenSparseMatrix::InnerIterator it(covariance,i); it; ++it) {
+	    m_cols.push_back(it.col());
+	    m_values.push_back(it.value());
+	    n_element++;
+	  }
+	}
+	
+	const std::vector<int> r_const(m_rows.begin(),m_rows.end());
+  const std::vector<int> c_const(m_cols.begin(),m_cols.end());
+  const std::vector<double> v_const(m_values.begin(),m_values.end());
+
+	ar(cereal::make_nvp("n_elements", n_element ));
+	ar(cereal::make_nvp("rows_data", r_const ));
+	ar(cereal::make_nvp("cols_data", c_const ));
+	ar(cereal::make_nvp("values_data", v_const ));
+	
+  }
+
+  template <class Archive>
+  void load( Archive & ar)
+  {
+	int n_rows;
+	int n_cols;
+	int n_element;
+	ar(cereal::make_nvp("rows", n_rows ));
+	ar(cereal::make_nvp("cols", n_cols ));
+	ar(cereal::make_nvp("n_elements", n_element ));
+	
+	std::vector<int> m_rows(n_rows+1);
+	std::vector<int> m_cols(n_element);
+	std::vector<double>m_values(n_element);
+  ar(cereal::make_nvp("rows_data", m_rows ));
+  ar(cereal::make_nvp("cols_data", m_cols ));
+  ar(cereal::make_nvp("values_data", m_values ));
+
+  covariance = Eigen::MappedSparseMatrix<double, Eigen::RowMajor>(
+  	n_rows, n_cols,
+  	static_cast<int>(n_element),
+  	m_rows.data(), m_cols.data(), m_values.data());
+
+  }
+};
+
+
+
+} // namespace sfm
+} // namespace openMVG
+
+#endif // OPENMVG_SFM_UNCERTAINTY_HPP
---
+++ src/software/SfM/main_EstimateUncertainty.cpp
@@ -0,0 +1,126 @@
+
+
+// Copyright (c) 2012, 2013, 2015 Pierre MOULON.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "openMVG/sfm/sfm.hpp"
+#include "openMVG/sfm/sfm_data_BA_ceres.hpp"
+#include "openMVG/cameras/cameras.hpp"
+#include "openMVG/sfm/sfm_data_io.hpp"
+#include "openMVG/cameras/Cameras_Common_command_line_helper.hpp"
+
+
+using namespace openMVG;
+using namespace openMVG::cameras;
+using namespace openMVG::sfm;
+
+#include "third_party/cmdLine/cmdLine.h"
+#include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
+#include "openMVG/numeric/numeric.h"
+
+
+int main(int argc, char **argv)
+{
+  CmdLine cmd;
+
+  std::string sSfM_Data_Filename;
+  std::string sIntrinsic_refinement_options = "ADJUST_ALL";
+  int i_User_camera_model = PINHOLE_CAMERA_RADIAL3;
+  bool bEstimateLandmarks = false;
+  std::string sOutDir = "";
+
+  cmd.add( make_option('i', sSfM_Data_Filename, "sfmdata") );
+  cmd.add( make_option('c', i_User_camera_model, "camera_model") );
+  cmd.add( make_option('f', sIntrinsic_refinement_options, "refineIntrinsics") );
+  cmd.add( make_option('l', bEstimateLandmarks, "estimateLandmarks") );
+  cmd.add( make_option('o', sOutDir, "outdir") );
+
+  try {
+      if (argc == 1) throw std::string("Invalid command line parameter.");
+      cmd.process(argc, argv);
+  } catch(const std::string& s) {
+      std::cerr << "Usage: " << argv[0] << '\n'
+      << "[-i|--sfmdata] filename, the SfM_Data file to use\n"
+      << "[-c|--camera_model] Camera model type for view with unknown intrinsic:\n"
+        << "\t 1: Pinhole \n"
+        << "\t 2: Pinhole radial 1\n"
+        << "\t 3: Pinhole radial 3 (default)\n"
+        << "\t 4: Pinhole radial 3 + tangential 2\n"
+        << "\t 5: Pinhole fisheye\n"
+      << "[-f|--refineIntrinsics] Intrinsic parameters refinement option\n"
+        << "\t ADJUST_ALL -> refine all existing parameters (default) \n"
+        << "\t NONE -> intrinsic parameters are held as constant\n"
+        << "\t ADJUST_FOCAL_LENGTH -> refine only the focal length\n"
+        << "\t ADJUST_PRINCIPAL_POINT -> refine only the principal point position\n"
+        << "\t ADJUST_DISTORTION -> refine only the distortion coefficient(s) (if any)\n"
+        << "\t -> NOTE: options can be combined thanks to '|'\n"
+        << "\t ADJUST_FOCAL_LENGTH|ADJUST_PRINCIPAL_POINT\n"
+        <<      "\t\t-> refine the focal length & the principal point position\n"
+        << "\t ADJUST_FOCAL_LENGTH|ADJUST_DISTORTION\n"
+        <<      "\t\t-> refine the focal length & the distortion coefficient(s) (if any)\n"
+        << "\t ADJUST_PRINCIPAL_POINT|ADJUST_DISTORTION\n"
+        <<      "\t\t-> refine the principal point position & the distortion coefficient(s) (if any)\n"
+      << "[-l|--estimateLandmarks] estimate uncertainty of landmarks\n"
+      << "[-o|--outdir] path\n"
+      << std::endl;
+
+      std::cerr << s << std::endl;
+      return EXIT_FAILURE;
+  }
+
+  if (sOutDir.empty())  {
+    std::cerr << "\nIt is an invalid output directory" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (i_User_camera_model < PINHOLE_CAMERA ||
+      i_User_camera_model > PINHOLE_CAMERA_FISHEYE )  {
+    std::cerr << "\n Invalid camera type" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  const cameras::Intrinsic_Parameter_Type intrinsic_refinement_options =
+    cameras::StringTo_Intrinsic_Parameter_Type(sIntrinsic_refinement_options);
+
+  std::cout << " You called : " <<std::endl
+            << argv[0] << std::endl
+            << "--sfmdata " << sSfM_Data_Filename << std::endl
+            << "--outdir " << sOutDir << std::endl;
+
+  // Create output dir
+  if (!stlplus::folder_exists(sOutDir))
+    stlplus::folder_create( sOutDir );
+
+  // Read the SfM scene
+  SfM_Data sfm_data;
+  if (!Load(sfm_data, sSfM_Data_Filename, ESfM_Data(ALL))) {
+    std::cerr << std::endl
+      << "The input SfM_Data file \""<< sSfM_Data_Filename << "\" cannot be read." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "\n\n-------------------------------" << "\n"
+      << "Estimate Uncertainty"<<"\n"
+      << "-------------------------------" << "\n";
+  Bundle_Adjustment_Ceres::BA_Ceres_options options;
+  Bundle_Adjustment_Ceres bundle_adjustment_obj(options);
+  const Optimize_Options ba_refine_options
+    ( intrinsic_refinement_options,
+      Extrinsic_Parameter_Type::ADJUST_ALL, // Adjust camera motion
+      Structure_Parameter_Type::ADJUST_ALL // Adjust scene structure
+    );
+  bundle_adjustment_obj.EstimateUncertainty(sfm_data, ba_refine_options,bEstimateLandmarks);
+
+
+  //-- Export to disk computed scene (data & visualizable results)
+  std::cout << "...Export SfM_Data to disk." << std::endl;
+  Save(sfm_data,
+    stlplus::create_filespec(sOutDir, "sfm_data", ".json"),
+    ESfM_Data(ALL|UNCERTAINTIES));
+
+
+  return EXIT_SUCCESS;
+}
